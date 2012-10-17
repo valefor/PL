bd6563d850a30c8eaa16e0909c96f08cd03db9e4
Herr_Alucard <adrain.f.tepes@gmail.com>
2012-10-17 17:57:53 +0800

    modified:   Console.cc 	modified:   Console.h 	modified:   Makefile

--- c++/Console.cc
+++ c++/Console.cc
@@ -1,6 +1,5 @@
 #include "Console.h"
 #include <string>
-//#include <boost/regex.hpp>
 
 Console::Console(Operable & opra){
     oprb = &opra;
@@ -11,33 +10,39 @@ void Console::start() {
     oprb->show();
 
     std::string cmd = "";
-    std::string arg = "";
+    boost::smatch matches;
 
     while (true)
     {
         std::cout << "(csl):";
         std::getline(std::cin,cmd);
+        CMD_TYPE ct = cmdFilter(cmd,matches);
+        
+        std::cout << "command:" << matches[1] << "\n";
 
-        if (!dispatch(cmdFilter(cmd),arg)) break;
+        if (!dispatch(ct,matches)) break;
     }
 }
 
-Console::CMD_TYPE Console::cmdFilter(std::string cmd) {
-    //boost::regex lsRegex("(ls|show|s)");
-    //boost::regex cdRegex("(goto|g|cd)");
-    //boost::regex qtRegex("(quit|q|exit)");
+Console::CMD_TYPE Console::cmdFilter(std::string cmd,boost::smatch & sm) {
+    boost::regex lsRegex("(ls|show|s)");
+    boost::regex cdRegex("(goto|g|cd)[ \t]*([0-9])+");
+    boost::regex qtRegex("(quit|q|exit)");
+    boost::regex undefRegex("([a-zA-Z]+).*");
 
-    //if(boost::regex_match(cmd,lsRegex)) return CT_LS;
-    //if(boost::regex_match(cmd,cdRegex)) return CT_CD;
-    //if(boost::regex_match(cmd,qtRegex)) return CT_QT;
+    if(boost::regex_match(cmd,sm,lsRegex)) return CT_LS;
+    if(boost::regex_match(cmd,sm,cdRegex)) return CT_CD;
+    if(boost::regex_match(cmd,sm,qtRegex)) return CT_QT;
     //if("ls" == cmd || "show"==cmd || "s" == cmd) return CT_LS;
     //if("goto" == cmd || "g"==cmd || "cd" == cmd) return CT_CD;
     //if("quit" == cmd || "q"==cmd || "exit" == cmd) return CT_QT;
 
-    return CT_UNDEF;
+    if(boost::regex_match(cmd,sm,undefRegex)) return CT_UNDEF;
+
+    return CT_ERR;
 }
 
-bool Console::dispatch(CMD_TYPE cmdType,std::string arg) {
+bool Console::dispatch(CMD_TYPE cmdType,boost::smatch & sm) {
 
     //std::cout << "command:" << cmd << std::endl;
     //if ( CT_QT == cmdType) return false;
@@ -45,9 +50,13 @@ bool Console::dispatch(CMD_TYPE cmdType,std::string arg) {
         case CT_QT: 
             return false;
         case CT_CD:
-            ;
+            break;
         case CT_LS:
-            ;
+            oprb->show();
+            break;
+        case CT_UNDEF:
+            std::cout << "Unrecognized command:" << sm.str(1) <<"\n";
+            break;
         default:
             ;
         
--- c++/Console.h
+++ c++/Console.h
@@ -1,13 +1,14 @@
 #include "Interface.h"
 #include <iostream>
+#include <boost/regex.hpp>
 
 class Console {
 
     Operable * oprb;
 
-    enum CMD_TYPE { CT_LS,CT_CD,CT_QT,CT_EX, CT_UNDEF };
-    bool dispatch(CMD_TYPE,std::string);
-    CMD_TYPE cmdFilter(std::string);
+    enum CMD_TYPE { CT_LS, CT_CD, CT_QT, CT_EX, CT_UNDEF, CT_ERR };
+    bool dispatch(CMD_TYPE,boost::smatch &);
+    CMD_TYPE cmdFilter(std::string,boost::smatch &);
 
     public:
     Console(Operable &);
