1b3165fd7f3862914b68bb391e7ae1e3835b718d
Herr_Alucard <adrain.f.tepes@gmail.com>
2013-10-11 23:21:18 +0800

    update

--- c++/algorithm/queen.cpp
+++ c++/algorithm/queen.cpp
@@ -1,12 +1,24 @@
 /*
  * r - row, c - column
+ *
+ *  N6:
  *r/c 1 2 3 4 5 6
- *  1 - x - - - - 
+ *  1 - x - - - -
  *  2 - - - x - - 
  *  3 - - - - - x 
  *  4 x - - - - - 
  *  5 - - x - - - 
  *  6 - - - - x - 
+ *
+ *  N7:
+ *r/c 1 2 3 4 5 6 7
+ *  1 x - - - - - -
+ *  2 - - x - - - - 
+ *  3 - - - - x - - 
+ *  4 - - - - - - x 
+ *  5 - x - - - - - 
+ *  6 - - - x - - - 
+ *  7 - - - - - x - 
  *
  * */
 
@@ -14,24 +26,30 @@
 #include <stdlib.h>
 
 class Chessboard {
-    int dimension;
+    int number;
     int cnt_solution;
     int ** result;
+    int result_size;
     // Markers
     bool * row, * xl, *xr;
 
     public:
-    Chessboard(int dim) : dimension(dim),cnt_solution(0) {
-        row = (bool *)malloc( sizeof(bool)*dimension );
-        xl = (bool *)malloc( sizeof(bool) * (dimension*2 -1) );
-        xr = (bool *)malloc( sizeof(bool) * (dimension*2 -1) );
-        result = (int **)malloc( sizeof(int) * dimension * dimension );
+    Chessboard(int n) : number(n),cnt_solution(0) {
+        //row = (bool *)malloc( sizeof(bool)*number );
+        //xl = (bool *)malloc( sizeof(bool) * (number*2 -1) );
+        //xr = (bool *)malloc( sizeof(bool) * (number*2 -1) );
+        //result = (int **)malloc( sizeof(int) * number * number );
+        row = new bool[number];
+        xl = new bool[number*2-1];
+        xr = new bool[number*2-1];
+        result = NULL;
+        resizeResult(number);
 
         // Init makers
         row[0] = xl[0] = xr[0] = true;
-        for (int i = 1; i < dimension ; i++) {
+        for (int i = 1; i < number ; i++) {
             row[i] = xl[i] = xr[i] = true;
-            xl[i+dimension-1] = xr[i+dimension-1] = true;
+            xl[i+number-1] = xr[i+number-1] = true;
         }
     }
 
@@ -47,35 +65,79 @@ class Chessboard {
 
     private:
     bool isAvailable(int r, int c) {
-        return row[c-1] && xr[c-1+r-1] && xl[c-1+dimension-r];
+        /*
+        std::cout << "row:" << r << "column:" << c << std::endl;
+        std::cout << "xr:" << c-1+r-1 << "[" << xr[c-1+r-1] << "]" << std::endl;
+        std::cout << "xl:" << c-1+number-r << "[" << xr[c-1+number-r] << "]" << std::endl;
+        if (row[c-1] && xr[c-1+r-1] && xl[c-1+number-r]) {
+            std::cout << "available!" << std::endl;
+        }
+        */
+        return row[c-1] && xr[c-1+r-1] && xl[c-1+number-r];
+    }
+
+    void resizeResult(int n) {
+        int old_size = result_size;
+
+        int ** result_new = new int*[n*number];
+        result_size = n*n;
+        result_new[0] = new int[result_size*n];
+        for (int i =1; i< result_size; i++) result_new[i] = result_new[i-1]+ number;
+
+        if(result != NULL) {
+            for (int i = 0; i < old_size; i++ ) result_new[i] = result[i];
+        }
+        delete [] result;
+        result = result_new;
+    }
+
+    void xxDebug() {
+        std::cout << "***XR-XL INFO BEGIN***" << std::endl;
+        
+        std::cout << "row[" ;
+        for ( int i = 0; i< number; i++ ) {
+            std::cout << row[i] << " ";
+        }
+        std::cout << "]" << std::endl;   
+        std::cout << "xr[" ;
+        for ( int i = 0; i< number*2-1; i++ ) {
+            std::cout << xr[i] << " ";
+        }
+        std::cout << "]" << std::endl;
+        std::cout << "xl[";
+        for ( int i = 0; i< number*2-1; i++ ) {
+            std::cout << xl[i] << " ";
+        }
+        std::cout << "]" << std::endl;
+        std::cout << "***XR-XL INFO END***" << std::endl;
     }
 
     void mark(int r, int c, bool reset=false) {
         row[c-1] = reset;
         xr[c-1+r-1] = reset;
-        xl[c-1+dimension-r] = reset;
+        xl[c-1+number-r] = reset;
     }
 };
 
 void Chessboard::solve(int r) {
-
-    if (r == dimension+1) {
+    if (r == number+1) {
         cnt_solution ++;
-        for (int i = 1; i <= dimension ; i++) {
-        result[cnt_solution][i] = result[cnt_solution-1][i];
+        if (cnt_solution >= result_size)
+            resizeResult(result_size);
+        for (int i = 0; i < number ; i++) {
+            result[cnt_solution][i] = result[0][i];
+            //print();
         }
+        return;
     }
 
-    for (int i = 1; i <= dimension ; i++) {
-        row[r] = i;
-        int temp = r - dimension;
-        if (temp < 0 ) temp = 0 - temp;
-        if ( isAvailable(i,r) ) {
-            mark(i,r);
-            result[cnt_solution][r] = i;
+    for (int i = 1; i <= number ; i++) {
+        if ( isAvailable(r,i) ) {
+            mark(r,i);
+            result[0][r-1] = i;
             // Deep First Search
             solve(r+1);
-            mark(i,r,true);
+            mark(r,i,true);
         }
     }
 }
@@ -90,22 +152,26 @@ void Chessboard::print() {
 
     for (int  i = 1 ; i <= cnt_solution ; i++)
     {
-        for (int i = 0; i < dimension ; i++) {
-            std::cout << result[cnt_solution][i] << " ";
+        for (int j = 0; j < number ; j++) {
+            std::cout << result[i][j] << " ";
         }
         std::cout << std::endl;
     }
+    std::cout << cnt_solution << std::endl;
 }
 
 int main(int argn, char ** args) {
 
-    int dimension;
+    int number = 0;
     //std::cout << argn << std::endl;
     //std::cout << args[1] << std::endl;
-    //std::cout << "Enter dimension:" << std::endl;
-    // std::cin >> dimension;
-
-    Chessboard * queen = new Chessboard(6);
+    while(true) {
+    std::cout << "Enter number of queens[1-9]:" << std::endl;
+    std::cin >> number;
+    if ( number > 0 && number < 10 ) break;
+    else std::cout << "The number must be shorter than 10 and bigger than 0" << std::endl;
+    }
+    Chessboard * queen = new Chessboard(number);
 
     queen->solve(1);
     queen->print();
