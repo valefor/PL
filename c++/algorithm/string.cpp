2b56c98d9f129b7fb1d09dafe774a347b957f618
Herr_Alucard <adrain.f.tepes@gmail.com>
2013-10-28 17:47:16 +0800

    update

--- c++/algorithm/string.cpp
+++ c++/algorithm/string.cpp
@@ -3,6 +3,7 @@
  * C language
  */
 #include <iostream>
+#include <stdlib.h>
 int length(char *);
 
 /*
@@ -22,33 +23,98 @@ int find(char * target, char * substr, int * startIdx) {
     int subLen = length(substr);
     
     if (*startIdx >= targetLen || subLen < 1 || *startIdx < 0) return -1;
-    char * pt = target+*startIdx, *ps = substr;
-    while (*pt != '\0') {
-        if (*ps == '\0') return (*startIdx)-subLen;
+    char * pt = target, *ps = substr;
+    int i = *startIdx,j = 0, ret = -1;
+    while (i <= (targetLen-subLen)) {
 
-        if (*pt == *ps) {
-            ps++;
+        if (pt[i] == ps[j]) {
+            i++, j++;
         } else {
-            ps = substr;
+            if (j != 0) {
+                i = i - j, j = 0;
+            } 
+            //std::cout << "pt[" << i << "]"<< pt[i] << ":" << ps[j] << std::endl;
+            i ++;
         }
-        pt++, (*startIdx)++;
-    }
-    if (*ps == '\0') return (*startIdx)-subLen;
 
-    return -1;
+        if (j == subLen) {
+            *startIdx = i;
+            ret = i-j;
+            break;
+        }
+    }
+
+    return ret;
 }
 
 // How to improve the performance?
-// 
+//  reduce regress steps when mismatch happened 
+//  KMP algorithm:
+//  Supose t means target string, s means src string,
+//  t[i] != s[i], that indicates t[0...i-1] == s[0...i-1]
+//  if there is a way to know s[0...j] == s[i-j-1...i-1], when mismatch happened,
+//  no need to regress to s[0] to start comparison again, start from s[j+1],
+//  here comparing s[j+1] and t[i] save about 'j' times of meanless comparison
+//  we need to evaluate the maximum 'j',
+//
 //
 // high performance version
-int eFind() {
+int* kmp(char * src, int n) {
 
+    int * next = (int*)malloc(sizeof(int)*n);
+
+    next[0] = -1;
+    //int i = 0;
+    for (int i = 0 ; i < n; i++) {
+        int max = 0; 
+        for (int k = 0; k < i; k++) {
+            if (src[k] == src[max]) {
+                k++,max++;
+            }
+        }
+        next[i] = max;
+    }
+
+    return next;
 }
 
-int length(char * target) {
+int kmpFind(char * target, char * substr, int * startIdx){
+    
+    int targetLen = length(target);
+    int subLen = length(substr);
 
-    char * p = target ;
+    if (*startIdx >= targetLen || subLen < 1 || *startIdx < 0) return -1;
+
+    int * nextv = kmp(substr, subLen);
+    int i = *startIdx, j = 0, ret = -1;
+    char * pt = target, *ps = substr;
+    while (i <= (targetLen-subLen)) {
+        
+        if (pt[i] == ps[j]) {
+            i++, j++;
+        } else {
+            if (nextv[j] == -1) {
+                i++, j == 0;
+            } else {
+                j = nextv[j];
+            }
+        }
+
+        if (j == subLen) {
+            *startIdx = i;
+            ret = i-j;
+            break;
+        }
+    }
+
+    // release nextv before return;
+    free(nextv);
+    return ret;
+}
+
+int length(char * str) {
+
+    char * p = str ;
     int i = 0;
 
     while ( *(p+i) != '\0') {
@@ -58,7 +124,7 @@ int length(char * target) {
 }
 
 int main () {
-    char * target = "The test string test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test";
+    char * target = "The tttts test string test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test";
     char * substr = "test";
 
     int pos = 0, startIdx = 0;
