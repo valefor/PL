fd898da1bcc9304a0cb6813de7349825584d6e69
Herr_Alucard <adrain.f.tepes@gmail.com>
2014-04-15 17:16:31 +0800

    rename directories

--- c++/modern_c++/singleton.cpp
+++ c++/modern_c++/singleton.cpp
@@ -1,4 +1,7 @@
+#include <mutex>
+
 #include "singleton.h"
+
 
 // Definition of static variable members here
 // Because static members belong to class instead of instance
@@ -6,12 +9,38 @@
 bool Singleton::destroyed_ = false;
 Singleton * Singleton::instance_  = nullptr;
 
+std::mutex mutex_;
+
+// In c++11, can also use std::lock_guard
+class Lock {
+    
+    std::mutex * mt_;
+    bool isLocked;
+
+    public:
+    Lock(std::mutex * mt) : mt_(mt) {
+        while( !(isLocked = mt_->try_lock()) ){}
+    }
+
+    ~Lock() {
+        if (isLocked) {
+            mt_->unlock();
+            isLocked = false;    
+        }
+    }
+};
+
 Singleton & Singleton::getInstance() {
     if(!instance_) {
-        if (destroyed_) {
-            onDeadReference();
-        } else {
-            createInstance();
+        // Double-checked locking
+        std::lock_guard<std::mutex> lock(mutex_);
+        // From now it's thread-safe.
+        if (!instance_) {
+            if (destroyed_) {
+                onDeadReference();
+            } else {
+                createInstance();
+            }
         }
     }
 
