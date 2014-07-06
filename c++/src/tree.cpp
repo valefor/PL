6dd0aeb92cb41f30c8579b758e6b5d8c482cfc9a
Herr_Alucard <adrain.f.tepes@gmail.com>
2014-07-07 01:00:45 +0800

    complete 'add' operation of radix

--- c++/src/tree.cpp
+++ c++/src/tree.cpp
@@ -136,8 +136,16 @@ bool RadixTree::updateSubTree(RadixInterNode * insert,
         // insertion happends on left side
         parent->left.iNode = insert;
     }
+
+    if (insert->bits == found->bits) {
+        // the left/right children of 'found' are re-parent to this 'insert'
+        // free it and return 
+        delete found;
+        return true;
+    }
     
     found->parent = insert;
+    return true;
 }
 
 // These 'link' functions are straightforward
@@ -155,24 +163,56 @@ void RadixTree::link(RadixInterNode * internal, RadixNode * node, RadixInterNode
 {
     if (internal==nullptr) return;
 
-    // the bits'th has be set in 'node' prefix, 'node' should on right side.
-    // otherwise, on the left side.
-    if (RadixBitTest(getNodePrefix(node), internal->bits)) {
-        internal->right.rNode = node;
-        internal->left.iNode = found;
-        // this internal node has an external node on right
-        internal->flags = FLAG_IN_R_EXT;
-    } else {
-        internal->left.rNode = node;
-        internal->right.iNode = found;
-        // this internal node has an external node on left
-        internal->flags = FLAG_IN_L_EXT;
-    }
+    // If user forget to set flag, set it here
+    if (internal->attached != nullptr) 
+        internal->flags = BIT_SET(internal->flags, FLAG_IN_ATTACHED);
 
-    // After linking these nodes together shall update the subtree
-    // The parent of 'found' should be the parent of this internal node
-    // and this internal node should be the new parent of 'found'
-    updateSubTree(internal,found);
+    // If the internal node has an attached node which means it's an 
+    // 'route' internal node
+    if (internal->flags && BIT_ISSET(internal->flags, FLAG_IN_ATTACHED)) {
+        // In this routine, 'node' is used to pass the external child of
+        // 'found', by doing this we know the 'found' is on which path.
+        if (found->bits == internal->bits) {
+            // [@Case4-2-1]: Replace 'found' with 'internal'
+            internal->left = found->left;
+            internal->right = found->right;
+            internal->flags = found->flags | FLAG_IN_ATTACHED;
+        } else if (RadixBitTest(getNodePrefix(node), internal->bits)) {
+            // [@Case4-2-2]: 'found' is on the right path of 'internal'
+            // that means its parent:found is the right child of 'internal'
+            // but the problem is who shall be the left child? we use attched one
+            internal->right.iNode = found;
+            internal->left.rNode = internal->attached;
+            internal->flags = internal->flags | FLAG_IN_L_ATTACHED | FLAG_IN_R_EXT;
+
+        } else {
+            // [@Case4-2-3]: 'Found' is on the left path of 'internal'
+            // that means its parent:found is the left child of 'internal'
+            internal->left.iNode = found;
+            internal->right.rNode = internal->attached;
+            internal->flags = internal->flags | FLAG_IN_R_ATTACHED | FLAG_IN_L_EXT;
+        }
+
+    // It's a 'pure' internal node
+    } else {
+        // the bits'th has be set in 'node' prefix, 'node' should on right side.
+        // otherwise, on the left side.
+        if (RadixBitTest(getNodePrefix(node), internal->bits)) {
+            internal->right.rNode = node;
+            internal->left.iNode = found;
+            // this internal node has an external node on right
+            internal->flags = FLAG_IN_R_EXT;
+        } else {
+            internal->left.rNode = node;
+            internal->right.iNode = found;
+            // this internal node has an external node on left
+            internal->flags = FLAG_IN_L_EXT;
+            // After linking these nodes together shall update the subtree
+            // The parent of 'found' should be the parent of this internal node
+            // and this internal node should be the new parent of 'found'
+            updateSubTree(internal,found);
+        }
+    }
 }
 
 /*              iNode[bits:x]  
@@ -185,19 +225,21 @@ void RadixTree::link(RadixInterNode * internal, RadixNode * node, RadixNode *fou
     if (internal==nullptr) return;
 
     // If user forget to set flag, set it here
-    if (internal->attached == found) 
+    if (internal->attached != nullptr) 
         internal->flags = BIT_SET(internal->flags, FLAG_IN_ATTACHED);
 
     // If the internal node has an attached node which means it's an 
     // 'route' internal node
     if (internal->flags && BIT_ISSET(internal->flags, FLAG_IN_ATTACHED)) {
-        if (RadixBitTest(getNodePrefix(node), internal->bits)) {
-            internal->right.rNode = node;
-            internal->left.rNode = found;
+        // In this routine, 'node' is useless since we can fetch it
+        // through internal->attached
+        if (RadixBitTest(getNodePrefix(found), internal->bits)) {
+            internal->right.rNode = found;
+            internal->left.rNode = internal->attached;
             internal->flags = internal->flags | FLAG_IN_L_ATTACHED | FLAG_IN_R_EXT;
         } else {
-            internal->left.rNode = node;
-            internal->right.rNode = found;
+            internal->left.rNode = found;
+            internal->right.rNode = internal->attached;
             internal->flags = internal->flags | FLAG_IN_R_ATTACHED | FLAG_IN_L_EXT;
         }
     
@@ -215,8 +257,6 @@ void RadixTree::link(RadixInterNode * internal, RadixNode * node, RadixNode *fou
             internal->right.rNode = found;
         }
     }
-
-    
 
     // After linking these nodes together shall update the subtree
     // The parent of 'found' should be the parent of this internal node
@@ -408,8 +448,6 @@ bool RadixTree::add(RadixNode * rNode)
              * and its parent(currNode, a internal node)
              */
             link(newInode, rNode, found);
-            // Last we should update/adjust the sub tree which currNode points to
-            // updateSubTree(currNode, found, newInode);
         } else {
             // [@Case1-2] : Insertion happends up the tree(currNode)
             RadixInterNode * parent = currNode->parent;
@@ -423,9 +461,6 @@ bool RadixTree::add(RadixNode * rNode)
             // Node to insert found, the node to be added is between this parent and
             // currNode 
             link(newInode, rNode, currNode);
-
-            // Last we should update/adjust the sub tree which currNode points to
-            // updateSubTree(parent, currNode, newInode);
         }
 
     /*
@@ -445,6 +480,48 @@ bool RadixTree::add(RadixNode * rNode)
      * minPrefixLen == found->prefixLength == diffBit
      */
     } else if (rNode->prefixLength > found->prefixLength) {
+        if (currNode &&(found->prefixLength == currNode->bits)) {
+            /*
+             * The external we found is an attached route.  
+             * There is guaranteed to be an invalid child
+             * pointer in the `curr' internal node that we can
+             * just point at our route.
+             */
+            rNode->parent = currNode;
+            if (RadixBitTest(getNodePrefix(rNode),currNode->bits)) {
+                currNode->right.rNode = rNode;
+                BIT_RESET(currNode->flags,FLAG_IN_R_ATTACHED);
+                BIT_SET(currNode->flags,FLAG_IN_R_EXT);
+            } else {
+                currNode->left.rNode = rNode;
+                BIT_RESET(currNode->flags,FLAG_IN_L_ATTACHED);
+                BIT_SET(currNode->flags,FLAG_IN_L_EXT);
+            }
+        } else {
+            /*
+             * The external we found is a child route.  Here
+             * we'll need to acquire a new internal node with space
+             * for an attached external, attach `found' to it and
+             * point one of the child pointers at `new', leaving the
+             * other side invalid.
+             */
+            RadixInterNode * newInode = new RadixInterNode();
+            newInode->bits = found->prefixLength;
+            newInode->attached = found;
+            newInode->flags = FLAG_IN_ATTACHED;
+
+            if (RadixBitTest(getNodePrefix(rNode), newInode->bits)) {
+                newInode->right.rNode = rNode;
+                newInode->left.rNode = newInode->attached;
+                newInode->flags = newInode->flags | FLAG_IN_L_ATTACHED | FLAG_IN_R_EXT;
+            } else {
+                newInode->left.rNode = rNode;
+                newInode->right.rNode = newInode->attached;
+                newInode->flags = newInode->flags | FLAG_IN_R_ATTACHED | FLAG_IN_L_EXT;
+            }
+
+            updateSubTree(newInode,found);
+        }
     
     /*
      * [@Case4]They have same minimum prefix and prefix length of node to be added is shorter
@@ -478,23 +555,7 @@ bool RadixTree::add(RadixNode * rNode)
             }
             // if ( (!parent || ( newInode->bits != parent->bits )))
             
-            if (currNode->bits == newInode->bits) {
-                // [@Case4-2-1]: Replace currNode with newInode
-                newInode->left = currNode->left;
-                newInode->right = currNode->right;
-                newInode->flags = currNode->flags | FLAG_IN_ATTACHED;
-            } else if (RadixBitTest(getNodePrefix(found)), newInode->bits) {
-                // [@Case4-2-2]: 'Found' is on the right path of 'newInode'
-                // that means its parent:currNode is the right child of 'newInode'
-                // but the problem is who shall be the left child? we use attched one
-
-            } else {
-                // [@Case4-2-3]: 'Found' is on the left path of 'newInode'
-                // that means its parent:currNode is the left child of 'newInode'
-            }
-            // Node to insert found, the node to be added is between this parent and
-            // currNode 
-            link(newInode, rNode, currNode);
+            link(newInode, found, currNode);
 
             // Last we should update/adjust the sub tree which currNode points to
             // updateSubTree(parent, currNode, newInode);
