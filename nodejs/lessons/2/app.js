7a3b8880c15d36be04309ce4e3c5858138bf928d
Adrian Hu <hufei1986818@gmail.com>
2014-10-29 11:28:44 +0000

    add web spider example

--- nodejs/lessons/2/app.js
+++ nodejs/lessons/2/app.js
@@ -1,9 +1,48 @@
+// Prerequisite:
+//  npm install --save express utility superagent cheerio 
+
 var express = require('express');
+var utility = require('utility');
+var superagent= require('superagent');
+var cheerio = require('cheerio');
+
 var app = express();
 
-app.get('/', function(req,res)
+app
+.get('/', function(req,res)
 {
-    res.send('Hello World!');
+    res.send("Hello World!");
+})
+.get('/md5', function(req,res)
+{
+    var q = req.query.q;
+    
+    var md5 = "Value:"+ q + "<br/>MD5:" + utility.md5(q);
+
+    res.send(md5);
+})
+.get('/spider', function(req,res,next)
+{
+    superagent.get('https://cnodejs.org/')
+    .end(function (err, sres) {
+        if (err) {
+            return next(err);
+        }
+        
+        // Load jQuery interface
+        var $ = cheerio.load(sres.text);
+        var items = [];
+        $('#topic_list .cell').each(function (idx, element) {
+            var $element = $(element);
+            items.push({
+                title:  $element.find('.topic_title').attr('title'),
+                href:   $element.find('.topic_title').attr('href'),
+                author: $element.find('img').attr('title')
+            });
+        });
+    
+        res.send(items);
+    });
 });
 
 app.listen(3000, function(){
